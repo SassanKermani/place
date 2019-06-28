eecc1e2b7a6bc5cabad00ed37f38c4e7bd70a90e
Sassan Kermani <sassankermani@Sassans-iMac.local>
2019-06-27 19:14:30 -0600

    kk

--- V1/server.js
+++ V1/server.js
@@ -15,11 +15,21 @@ http.createServer( (req, res)=>{
 	console.log(`req.method: ${req.method}`);
 
 	switch(true){
-
+		case(req.url == `/favicon.ico`):
+			console.log(`favicon.ico`)
+			break
 		case(req.url == `/` && req.method == `GET`):
 			homePage(req, res);
 			break;
-
+		case(req.url == `/addData` && req.method == `POST`):
+			 addData(req, res);
+			break;
+		case(req.url == `/getData` && req.method == `POST`):
+			 getData(req, res);
+			break;
+			case(req.url == `/getFile` && req.method == `POST`):
+			 getFile(req, res);
+			break;
 		default :
 			console.log(`broke`);
 	}
@@ -30,10 +40,54 @@ http.createServer( (req, res)=>{
 /*=====  End of Router  ======*/
 
 function homePage(req, res){
-	fs.readFile(`./homePage.html`, (err, data)=>{
+	fs.readFile(`./homePageV2.html`, (err, data)=>{
 		if(err){throw err};
-		console.log(`saved`);
 		res.end(data);
 	});
 
+}
+
+function addData(req, res){
+	let body = [];
+	req.on('data', (chunk) => {
+		body.push(chunk);
+	}).on('end', () => {
+		
+		body = JSON.parse(Buffer.concat(body).toString());
+		console.log(body);
+
+		// fs.writeFile(`data/${body.name}@${Math.random()}.txt`, JSON.stringify(body), (err) => {
+			fs.writeFile(`data/${body.name}.txt`, JSON.stringify(body), (err) => {
+			if (err) throw err;
+			console.log('The file has been saved!');
+		});
+
+		res.end(`Saved`);
+	});	
+}
+
+function getData(req, res){
+	fs.readdir(`./data`,(err, file)=>{
+		res.end(JSON.stringify({arr : file}));
+		console.log(`file`);
+		console.log(file);
+	});
+}
+
+function getFile(req, res){
+	let body = [];
+	req.on('data', (chunk) => {
+		body.push(chunk);
+	}).on('end', () => {
+		
+		body = JSON.parse(Buffer.concat(body).toString());
+		console.log(body);
+
+		fs.readFile(`./data/${body.file}`, (err, data)=>{
+			if(err){throw err};
+			console.log(typeof data);
+			res.end(data);
+		});
+
+	})
 }
